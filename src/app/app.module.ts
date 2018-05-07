7e61bd8ed518a727cf06091aac3b20b5ac7274e5
novakvova <novakvova@gmail.com>
2018-05-07 17:57:07 +0300

    Begin angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatInputModule, MatCardModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, QuestionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    BrowserAnimationsModule, 
+    MatButtonModule, 
+    MatInputModule, 
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
